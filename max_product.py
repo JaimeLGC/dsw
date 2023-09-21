912bf201298cc4bf4d84a9a507ce4885582fd7fe
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-09-21 12:28:44 +0100

    Se añade versión de prueba max_product

--- max_product.py
+++ max_product.py
@@ -1,36 +1,40 @@
-import sys
 
-number = sys.argv[1]
-distance = int(sys.argv[2])
+def max_product(number: str, distance: str):
+    try:
+        int(number)
+        distance = int(distance)
+    except:
+        raise ValueError('Los argumentos han de ser numéricos')
+    if distance >= len(number):
+        raise ValueError('El segundo argumento no puede ser mayor que el primero')
+    if distance <= 0:
+        raise ValueError('El segundo argumento no puede ser menor o igual a 0')
+    
+    max_res = 0
+    for i in range(len(number) - distance + 1):
+        res = 1
+        for n in number[i:i+distance]:
+            res *= int(n)
+        if res > max_res: 
+            max_res = res
+    return max_res
 
-try:
-    int(number)
-except:
-    raise ValueError('El primer argumento ha de ser un entero')
+# print(max_product('2357898', '3'))
+# print(max_product('23578985', '3'))
+# print(max_product('2357898', '15'))
+# print(max_product('2357898', 'a'))
+# print(max_product('23578b98', '5'))
+print(max_product('2357898', 3))
+# print(max_product(2357898, '3'))
+# print(max_product(2357898, 3))
 
-if distance >= len(number):
-    raise ValueError('El segundo argumento no puede ser mayor que el primero')
+# def max_product(number: str, distance: int, i: int, max_res: int):
+#     res = 1
+#     for n in number[i:i+distance]:
+#         res *= int(n)
+#     if res > max_res: 
+#         max_res = res
+#     i += 1  
+#     return max_product(number, distance, i, max_res) if (len(number) - i >= distance) else max_res
 
-if distance <= 0:
-    raise ValueError('El segundo argumento no puede ser menor o igual a 0')
-
-def max_product(number: str, distance: int, i: int, max_res: int):
-    res = 1
-    for n in number[i:i+distance]:
-        res *= int(n)
-    if res > max_res: 
-        max_res = res
-    i += 1  
-    return max_product(number, distance, i, max_res) if (len(number) - i >= distance) else max_res
-
-print(max_product(number, distance, 0, 0))
-
-# def max_res(number: int, distance: int):
-#     max_res = 0
-#     for i in range(len(number) - distance + 1):
-#         res = 1
-#         for n in number[i:i+distance]:
-#             res *= int(n)
-#         if res > max_res: 
-#             max_res = res
-#     return max_res
+# print(max_product(number, distance, 0, 0))
